bbcb582cd99405ae4fb530f4860e086a4d8b3b0b
Carlos Villalta <CarlosFindley@protonmail.com>
2018-11-09 23:56:21 -0800

    testing with Slick

---
+++ resources/scripts/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+
+
+
+
+
+    $(".slideshow").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      })
+
+  });
